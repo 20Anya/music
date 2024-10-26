b6c9881fbfb34e0e77df9893e72eeb23404d3589
Anya <aana45751@gmail.com>
2024-10-26 21:23:21 +0300

    Update README.sql

--- README.sql
+++ README.sql
@@ -1,14 +1,11 @@
 CREATE TABLE IF NOT EXISTS Performers (
     	id SERIAL PRIMARY KEY,
-	name varchar(40) NOT NULL,
-	genre varchar(40) NOT NULL,
-	album varchar(40) NOT NULL
+	name varchar(40) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Genres (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL,
-	performer VARCHAR(60) NOT NULL
+	name VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS PerformersGenres (
@@ -20,26 +17,20 @@ CREATE TABLE IF NOT EXISTS PerformersGenres (
 CREATE TABLE IF NOT EXISTS Albums (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(40) NOT NULL,
-	year_of_release INTEGER NOT NULL,
-	performer VARCHAR(60),
-	track VARCHAR(50)
+	year_of_release INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Tracks (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50),
 	length_of_the_song INTEGER NOT NULL,
-	performer VARCHAR(40) NOT NULL,
-	genre VARCHAR(40) NOT NULL,
-	collection VARCHAR(60) NOT NULL,
 	album_id INTEGER NOT NULL REFERENCES Albums(id)
 );
 
 CREATE TABLE IF NOT EXISTS Collections (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50),
-	year_of_release int not null,
-	track VARCHAR(50)
+	year_of_release INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS CollectionTracks (
